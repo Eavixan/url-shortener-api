2ad9b78f0562b8327f0b7aecdf13a229030247da
favyan-ai <kinsomerance2004@gmail.com>
2026-06-15 21:57:22 +0600

    Add retrieve short URL endpoint

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -2,54 +2,80 @@ const crypto = require("crypto");
 const Url = require("../models/Url");
 
 const isValidUrl = (url) => {
-  try {
-    new URL(url);
-    return true;
-  } catch {
-    return false;
-  }
+    try {
+        new URL(url);
+        return true;
+    } catch {
+        return false;
+    }
 };
 
 const generateShortCode = () => {
-  return crypto.randomBytes(4).toString("hex");
+    return crypto.randomBytes(4).toString("hex");
 };
 
 const createShortUrl = async (req, res) => {
-  try {
-    const { url } = req.body;
+    try {
+        const { url } = req.body;
 
-    if (!url) {
-      return res.status(400).json({
-        message: "URL is required",
-      });
+        if (!url) {
+            return res.status(400).json({
+                message: "URL is required",
+            });
+        }
+
+        if (!isValidUrl(url)) {
+            return res.status(400).json({
+                message: "Please provide a valid URL",
+            });
+        }
+
+        let shortCode = generateShortCode();
+
+        while (await Url.findOne({ shortCode })) {
+            shortCode = generateShortCode();
+        }
+
+        const newUrl = await Url.create({
+            url,
+            shortCode,
+        });
+
+        res.status(201).json(newUrl);
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error",
+            error: error.message,
+        });
     }
+};
 
-    if (!isValidUrl(url)) {
-      return res.status(400).json({
-        message: "Please provide a valid URL",
-      });
+const getOriginalUrl = async (req, res) => {
+    try {
+        const { shortCode } = req.params;
+
+        const urlData = await Url.findOneAndUpdate(
+            { shortCode },
+            { $inc: { accessCount: 1 } },
+            { new: true }
+        );
+
+        if (!urlData) {
+            return res.status(404).json({
+                message: "Short URL not found",
+            });
+        }
+
+        res.status(200).json(urlData);
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error",
+            error: error.message,
+        });
     }
-
-    let shortCode = generateShortCode();
-
-    while (await Url.findOne({ shortCode })) {
-      shortCode = generateShortCode();
-    }
-
-    const newUrl = await Url.create({
-      url,
-      shortCode,
-    });
-
-    res.status(201).json(newUrl);
-  } catch (error) {
-    res.status(500).json({
-      message: "Server error",
-      error: error.message,
-    });
-  }
 };
 
 module.exports = {
-  createShortUrl,
+    createShortUrl,
+    getOriginalUrl,
 };
